a64b898fd0916d64950487231479e036a0eab5eb
Piñón Ferrer <luis.pinon-ferrer@capgemini.com>
2018-11-08 15:58:02 +0100

    [AsyncHandling] Reducer for Ajax Status

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import courses from './courseReducer';
 import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
   courses,
   authors,
+  ajax,
+  ajaxCallsInProgress,
 });
 
 export default rootReducer;
