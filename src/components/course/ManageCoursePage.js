172faa3265fc4bdb791a1b357bfa9e286c243c5c
Piñón Ferrer <luis.pinon-ferrer@capgemini.com>
2018-11-08 14:02:02 +0100

    [ReduxAsyncWrites] Populating form data Mapping State to Props

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -51,7 +51,14 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object,
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter((course) => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
   let course = {
     id: '',
     watchHref: '',
@@ -60,6 +67,10 @@ function mapStateToProps(state, ownProps) {
     length: '',
     category: '',
   };
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map((author) => {
     return {
